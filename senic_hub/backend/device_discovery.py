ac361ca43060112e4bffda6d00b994141a5c2413
Avichal <pandeyavichal7@gmail.com>
2018-02-07 19:27:48 +0530

    add api for fetching Phue bridge config

--- senic_hub/backend/device_discovery.py
+++ senic_hub/backend/device_discovery.py
@@ -293,6 +293,11 @@ class PhilipsHueBridgeApiClient:
         url = "{}/{}/lights".format(self.bridge_url, self.username)
         return self._request(url)
 
+    @username_required
+    def get_config(self):  # pragma: no cover
+        url = "{}/{}/config".format(self.bridge_url, self.username)
+        return self._request(url)
+
 
 class SonosSpeakerDeviceDescription:
     def __init__(self, device_info):
